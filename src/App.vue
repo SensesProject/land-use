1809ab423b2ee4d0125bfcf2ab9b63bc0e5de772
Fidel Thomet <me@fidelthomet.com>
2020-01-06 11:49:23 +0100

    [Update] Intro

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
+    <SensesMenu/>
+    <article>
+      <Intro/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </article>
   </div>
 </template>
 
 <script>
+import SensesMenu from 'library/src/components/SensesMenu.vue'
+import Intro from './views/Intro.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    SensesMenu,
+    Intro,
     HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
+@import "library/src/style/base.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  article {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .text {
+      width: 100vw;
+      max-width: calc(768px + #{$spacing});
+      padding: $spacing $spacing / 2 0;
+    }
+  }
 }
 </style>
