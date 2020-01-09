ca7b5d8f202c487f0e4b9a25db792796268d3f91
Fidel Thomet <me@fidelthomet.com>
2020-01-09 15:50:40 +0100

    [Preprocessing] removed duplicated variables

--- preprocessing/landpressure.jl
+++ preprocessing/landpressure.jl
@@ -1,9 +1,7 @@
 using DataFrames, CSV, Statistics, StatsBase
 
 df = CSV.read("./input/validation.mif"; delim=";", missingstring="N/A")
-vars = CSV.read("./input/var_selection.csv"; delim=";")
-
-df[df.Variable .== "Consumption", :]
+vars = CSV.read("./input/var_selection.csv")
 
 # remove last (empty) column
 delete!(df, names(df)[end])
@@ -30,12 +28,26 @@ df = df[.&(df.year .>= 1960, df.year .<= 2019), :]
 
 df = unstack(df, :year, :value)
 
+# replace missing values with empty strings
+df[ismissing.(df.scenario), :scenario] .= ""
+df[ismissing.(df.model), :model] .= ""
+
+# filter to remove duplicates
+df = df[df.model .!= "Bodirsky2015", :]
+df = df[df.model .!= "FAO_crop_past", :]
+df = df[df.model .!= "FAOmassbalance", :]
+df = df[df.scenario .!= "projection", :]
+# df = df[df.model .!= "MAgPIEown", :]
+
+df = df[.!.&(df.model .== "MAgPIEown", df.variable .!= "Resources|Land Cover|Forest|+|Managed Forest", df.variable .!= "Resources|Land Cover|Forest|+|Natural Forest"), :]
+df = df[.!.&(df.model .== "Bodirsky", df.variable .== "Resources|Nitrogen|Cropland Budget|Inputs|+|Fertilizer"), :]
+df = df[.!.&(df.variable .== "Income", df.unit .!= "US\$05 PPP/cap/yr"), :]
+df = df[.|(df.model .!= "EDGAR_LU", df.variable .== "Emissions|CO2|Land|+|Land Use Change"), :]
+
+sort!(df, [order(:variable), order(:model), order(:scenario)])
 CSV.write("./output/landpressure.csv", df)
 
-# variables = DataFrame(sort(collect(countmap(df[:, :variable])), by = tuple -> last(tuple), rev=true))
-# names!(variables, [:variable, :count])
-
-# df[in(df.variable .== vars[:, 1]),:]
+# check for duplicates
 
 nonunique(df, :variable)
 dupVars = unique(df[nonunique(df, :variable), :variable])
@@ -43,9 +55,3 @@ dfDupVars = df[map(v -> in(v, dupVars[:, 1]), df.variable), :]
 sort!(dfDupVars, [order(:variable), order(:model), order(:scenario)])
 
 CSV.write("./output/landpressure-duplicates.csv", dfDupVars)
-
-CSV.write("./output/landpressure-uniques.csv", unique(df, :variable))
-
-head(df)
-
-df[6, 170:200]
