0f137c8b5c999fec7912cc0d914d193d2fdcfb44
Fidel Thomet <me@fidelthomet.com>
2020-04-22 18:07:54 +0200

    [Update] net comparison

--- src/components/ChartNet.vue
+++ src/components/ChartNet.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="chart-net narrow" :style="{width: `${width}px`}">
-    <svg :width="size" :height="size">
-      <rect v-for="(d, i) in dims" :key="`r-${i}`" :class="[d.color]" :transform="`translate(${i * dimWidth + 1} ${size / 2 - 1})`"
-        :width="dimWidth - 2" height="2"/>
-      <polyline v-for="(d, i) in dims" :key="`d-${i}`" :class="[d.color]" :transform="`translate(${i * dimWidth} ${size / 2 + d.yOffset})`"
-        :points="`1 0 ${dimWidth / 2} ${d.y} ${dimWidth - 1} 0`"/>
+    <svg :width="width" :height="height">
+      <g v-if="label" :transform="`translate(12 ${height / 2}) rotate(-90)`">
+        <text :x="height/ 4" text-anchor="middle">Gains</text>
+        <text :x="-height/ 4" text-anchor="middle">Needs</text>
+      </g>
+      <line class="ref" :x2="width"/>
+      <line class="ref" :x2="width" :y1="height/ 2" :y2="height/ 2"/>
+      <line class="ref" :x2="width" :y1="height" :y2="height"/>
+      <g :transform="`translate(${(width - size) / 2} 0)`">
+        <rect v-for="(d, i) in dims" :key="`r-${i}`" :class="[d.color]" :transform="`translate(${i * dimWidth + 1} ${height / 2 - 1})`"
+          :width="dimWidth - 2" height="2"/>
+        <g v-for="(d, i) in dims" :key="`d-${i}`" :transform="`translate(${i * dimWidth} ${height / 2 + d.yOffset})`">
+          <polyline v-if="d.y2 != null" class="max" :class="[d.color]" :points="`1 0 ${dimWidth / 2} ${d.y2} ${dimWidth - 1} 0`"/>
+          <polyline :class="[d.color]" :points="`1 0 ${dimWidth / 2} ${d.y} ${dimWidth - 1} 0`"/>
+        </g>
+        <g v-if="explain" :transform="`translate(${dimWidth / 2} ${height / 2})`">
+          <line class="explain" y1="-53" y2="-53" :x2="dimWidth / 2 + 3"/>
+          <line class="explain" y1="-18" y2="-18" :x2="dimWidth / 2 + 3"/>
+          <text y="-50" :x="dimWidth / 2 + 6">maximal</text>
+          <text y="-15" :x="dimWidth / 2 + 6">realistic</text>
+        </g>
+      </g>
       <!-- <polyline :points="`${size - size/6} ${size/2} ${size - size/6 * 2} ${size/2 + (size / 2 * data.cost)} ${size - size/6 * 3} ${size/2}`" class="violet"/> -->
     </svg>
     <span class="tiny">{{ data.name }}</span>
@@ -29,38 +46,53 @@ export default {
           color: 'green',
           key: 'removal'
         }, {
-          color: 'red',
-          key: 'land'
-        }, {
           color: 'yellow',
           key: 'energy'
+        }, {
+          color: 'violet',
+          key: 'cost'
         }, {
           color: 'blue',
           key: 'water'
         }, {
-          color: 'violet',
-          key: 'cost'
+          color: 'red',
+          key: 'land'
         }]
       }
+    },
+    label: {
+      type: Boolean,
+      default: false
+    },
+    explain: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      height: 160
     }
   },
   computed: {
     size () {
       const { width } = this
-      return Math.min(160, width)
+      return Math.min(160, width - 32)
     },
     dimWidth () {
       const { size, dims } = this
       return size / dims.length
     },
     dims () {
-      const { data, size, dimensions } = this
+      const { data, height, dimensions } = this
       return dimensions.map(d => {
         const offset = 3
-        const y = data[d.key] * (size / 2 - offset) * (d.key === 'removal' ? -1 : 1)
+        const y = data.realistic[d.key] * (height / 2 - offset) * (d.key === 'removal' ? -1 : 1)
+        const y2 = data.maximum != null ? data.maximum[d.key] * (height / 2 - offset) * (d.key === 'removal' ? -1 : 1) : null
         return {
           ...d,
           y,
+          y2,
           yOffset: y < 0 ? -offset : offset
         }
       })
@@ -80,13 +112,27 @@ export default {
     overflow: visible;
     polyline, rect {
       stroke: none;
-      @include tint(fill)
+      @include tint(fill);
+      &.max {
+        opacity: 0.2;
+      }
+    }
+    .ref {
+      stroke: $color-gray;
+      stroke-dasharray: 2 2;
+    }
+    .explain {
+      stroke: $color-green;
+      stroke-dasharray: 2 2;
+    }
+    g text {
+      fill: $color-dark-gray;
     }
   }
   span {
     // font-weight: $font-weight-bold;
     max-width: 200px;
-    padding: $spacing / 4 0 0 0;
+    padding: $spacing / 2 0;
     text-align: center;
   }
 }
--- src/components/VisNet.vue
+++ src/components/VisNet.vue
@@ -1,49 +1,18 @@
 <template>
   <div class="vis-net narrow" v-resize:debounce.initial="onResize" ref="visNet">
-    <div class="key tiny">
-      <svg width="12" height="12">
-        <polyline points="0 12 6 0 12 12" class="violet"/>
-      </svg>
-      <span>Gains</span>
-      <svg width="12" height="12">
-        <polyline points="0 0 6 12 12 0" class="violet"/>
-      </svg>
-      <span>Requierements</span>
-    </div>
     <div class="key tiny last">
-      <div>
-        <svg width="12" height="12">
-          <rect width="12" height="12" class="green"/>
-        </svg>
-        <span>Emission Removal</span>
-      </div>
-      <div>
-        <svg width="12" height="12">
-          <rect width="12" height="12" class="red"/>
-        </svg>
-        <span>Land</span>
-      </div>
-      <div>
-        <svg width="12" height="12">
-          <rect width="12" height="12" class="yellow"/>
-        </svg>
-        <span>Energy</span>
-      </div>
-      <div>
-        <svg width="12" height="12">
-          <rect width="12" height="12" class="blue"/>
-        </svg>
-        <span>Water</span>
-      </div>
-      <div>
-        <svg width="12" height="12">
-          <rect width="12" height="12" class="violet"/>
-        </svg>
-        <span>Costs</span>
-      </div>
+      <span class="highlight no-hover green">Emission removal</span><span v-html="`&nbsp;`"/>
+      <span class="highlight no-hover yellow">energy</span><span v-html="`&nbsp;`"/>
+      <span class="highlight no-hover purple">capital</span><span v-html="`&nbsp;`"/>
+      <span class="highlight no-hover blue">water</span>
+      and<span v-html="`&nbsp;`"/>
+      <span class="highlight no-hover red">land</span>
+      gained and needed by different negative emission technoligies realtive to BECCS. Light colors show the potential of a maximal, saturated colors of a realistic implementation.
     </div>
     <div class="charts">
-      <ChartNet v-for="(n, i) in nets" :key="`n-${i}`" :data="n" :width="width"/>
+      <ChartNet v-for="(n, i) in nets" :key="`n-${i}`"
+        :data="n" :dimensions="dimensions" :width="width"
+        :label="i === 0 || cols === 2 && i === 2" :explain="i === 1"/>
     </div>
   </div>
 </template>
@@ -52,7 +21,7 @@ import Nets from 'dsv-loader!@/assets/data/NegativeEmissionTechnoligies.csv' // 
 import ChartNet from '@/components/ChartNet.vue'
 import resize from 'vue-resize-directive'
 export default {
-  name: 'VisPressure',
+  name: 'vis-net',
   components: {
     ChartNet
   },
@@ -60,34 +29,72 @@ export default {
     resize
   },
   data () {
-    const cols = ['removal', 'land', 'water', 'energy', 'cost']
+    const dimensions = [{
+      color: 'green',
+      key: 'removal'
+    }, {
+      color: 'yellow',
+      key: 'energy'
+    }, {
+      color: 'purple',
+      key: 'cost'
+    }, {
+      color: 'blue',
+      key: 'water'
+    }, {
+      color: 'red',
+      key: 'land'
+    }]
+    const nets = ['BECCS',
+      'Afforestation & Reforrastation',
+      'Soil Carbon Storage',
+      'Biochar Addition to Soil'
+    ]
     const max = {}
-    cols.forEach(c => {
-      max[c] = Math.max(...Nets.map(d => Math.abs(d[c])))
+    dimensions.forEach(c => {
+      // max[c] = Math.max(...Nets.map(d => Math.abs(d[c])))
+      max[c.key] = Math.abs(Nets[0][c.key])
     })
-    const nets = Nets.map(n => {
-      const obj = {
-        name: n.name
-      }
-      cols.forEach(c => {
-        obj[c] = n[c] / max[c]
-      })
-      return obj
-    })
+    // const nets = Nets.map(n => {
+    //   const obj = {
+    //     name: n.name
+    //   }
+    //   cols.forEach(c => {
+    //     obj[c] = n[c] / max[c]
+    //   })
+    //   return obj
+    // })
     return {
       width: 320,
       columnSpacing: 16,
-      nets
+      cols: 2,
+      nets: nets.map(name => {
+        const realistic = Nets.find(n => n.name === name && n.cat === 'realistic')
+        const maximum = Nets.find(n => n.name === name && n.cat === 'maximum')
+        const obj = {
+          name,
+          realistic: {},
+          maximum: maximum ? {} : null
+        }
+        dimensions.forEach(c => {
+          obj.realistic[c.key] = realistic[c.key] / max[c.key]
+          if (maximum != null) {
+            obj.maximum[c.key] = maximum[c.key] / max[c.key]
+          }
+        })
+        return obj
+      }),
+      dimensions
     }
   },
   computed: {
   },
   methods: {
     onResize (a, b, c) {
-      const { columnSpacing } = this
+      // const { columnSpacing } = this
       const iw = this.$refs.visNet.getBoundingClientRect().width
-      const cols = iw < 480 ? 2 : 4
-      this.width = (iw - columnSpacing) / cols - columnSpacing // * 3
+      this.cols = iw < 480 ? 2 : 4
+      this.width = (iw) / this.cols // - columnSpacing // * 3
     }
   }
 }
@@ -107,38 +114,9 @@ export default {
     align-items: flex-start;
   }
   .key {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    justify-content: center;
-
-    &.last {
-      padding-top: $spacing / 8;
-    }
-
-    div {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    span {
-      margin-right: $spacing / 4;
-
-      &.before-line-key {
-        margin-right: $spacing / 2;
-      }
-    }
-    svg {
-      margin-right: $spacing / 8;
-      rect {
-        @include tint(fill);
-      }
-    }
-    .line-key {
-      white-space: nowrap;
-      display: flex;
-      align-items: center;
-    }
+    text-align: center;
+    max-width: 460px;
+    width: 100%;
   }
 }
 </style>
