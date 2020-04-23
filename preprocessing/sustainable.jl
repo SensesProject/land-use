9dd06f722b193a47a47895c472b0b438ac3dcf46
Fidel Thomet <me@fidelthomet.com>
2020-04-23 15:02:07 +0200

    [Update] data for sustainability

---
+++ preprocessing/sustainable.jl
@@ -0,0 +1,20 @@
+using DataFrames, CSV, Statistics, StatsBase
+
+df = CSV.read("./input/sustainable.csv";)
+vars = CSV.read("./input/sustainable_vars.csv")
+
+# remove last row 🙄
+df = df[.!ismissing.(df.Scenario),:]
+# remove region col
+select!(df, Not(:Region))
+df = stack(df, 5:size(df)[2])
+rename!(df, [:year, :value, :model, :scenario, :variable, :unit])
+df[occursin.("N2O", df.variable), :value] /= 1000
+df[occursin.("N2O", df.variable), :unit] = "Mt N₂O/yr"
+df[occursin.("CO2", df.variable), :unit] = "Mt CO₂/yr"
+df[occursin.("CH4", df.variable), :unit] = "Mt CH₄/yr"
+df = unstack(df, :year, :value)
+
+# sort!(df, [order(:variable, rev=true), order(:scenario)])
+df = join(vars,df,on = :variable, kind= :outer)
+CSV.write("../src/assets/data/sustainable.csv", df)
